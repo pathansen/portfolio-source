a40afecee024259ba8f407223548c8ed2d5fd7f6
Patrick <hansenp2@tcnj.edu>
2019-06-04 09:33:21 -0400

    component to represent projects

---
+++ src/components/Project.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import ProjectModal from './ProjectModal'
+import classes from './Project.module.css'
+
+class Project extends React.Component {
+
+  state = {
+    displayModal: false
+  }
+
+  toggleModalHandler = () => {
+    const updateDisplayModal = !this.state.displayModal;
+    this.setState({ displayModal: updateDisplayModal });
+  }
+
+  render() {
+
+    const { projectName, projectLanguage, projectRepoLink} = this.props;
+    const { displayModal } = this.state;
+
+    return (
+      <div>
+
+        {displayModal &&
+          <ProjectModal
+            toggleModal={this.toggleModalHandler}
+            projectName={projectName}
+            projectLanguage={projectLanguage}
+            projectRepoLink={projectRepoLink} />
+        }
+
+        <div className={classes.projectItem} onClick={this.toggleModalHandler}>
+          <div className={classes.titleCard}>
+            <h4 style={{margin:"0"}}>{projectName}</h4>
+            <h6 style={{margin:"0", fontWeight:"normal"}}>({projectLanguage})</h6>
+          </div>
+            <a className={classes.repositoryLink} href={projectRepoLink}>
+              <div href={projectRepoLink}>
+                <h6 style={{margin:"0", fontWeight:"normal"}}>GitHub Repository</h6>
+              </div>
+            </a>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Project;
